460ac3e78527df5bf94d3b967132258d71403520
Ayberk Gezer <43863521+ayberkgezer@users.noreply.github.com>
2025-05-10 01:53:07 +0300

    Initialize API routes and add health endpoint

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/ayberkgezer/gmail-smtp-fiber/internal/app/router"
 	"github.com/ayberkgezer/gmail-smtp-fiber/internal/server"
 	"github.com/gofiber/fiber/v2"
 )
@@ -8,6 +9,7 @@ import (
 func main() {
 	app := fiber.New()
 
+	router.InitializeRoutes(app)
 	// Initialize the server
 	server.NewServer(app).StartHttpServer()
 }
--- internal/app/router/router.go
+++ internal/app/router/router.go
@@ -1,7 +1,17 @@
 package router
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/ayberkgezer/gmail-smtp-fiber/internal/app/base"
+	"github.com/gofiber/fiber/v2"
+)
 
 func InitializeRoutes(app *fiber.App) {
+	api := app.Group("/api/v1")
 
+	// GET /api/v1/health
+	api.Get("/health", func(c *fiber.Ctx) error {
+		return c.Status(fiber.StatusOK).JSON(base.NewBaseResponse(fiber.StatusOK, "Server Running"))
+	})
+	// GET /api/v1/email/send
+	//api.Get("/email/send")
 }
